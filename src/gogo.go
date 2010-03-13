de6d11ef23e751c2f8f6aeb007b4a9734dd625f0
Michael Lippautz <michael.lippautz@gmail.com>
2010-03-13 21:02:36 +0100

    gogo: Main file performing test calls

--- src/gogo.go
+++ src/gogo.go
@@ -6,8 +6,19 @@ package main
 
 import "fmt"
 import "os"
-import "unsafe"
-import "syscall"
+//import "unsafe"
+//import "syscall"
+import "./libgogo/_obj/libgogo"
+
+func gogo_strcpy (s string) [255]byte {
+    var b [255]byte;
+    var i int;
+    for i=0;i<len(s);i++ {
+        b[i] = s[i]
+    }
+    b[i] = 0;
+    return b;
+}
 
 func main() {
 
@@ -16,15 +27,22 @@ func main() {
 		return;	
 	}
     
-	var r0 uintptr;
-	var e1 uintptr;
-	r0, _, e1 = syscall.Syscall(syscall.SYS_OPEN, uintptr(unsafe.Pointer(syscall.StringBytePtr(os.Args[1]))), 0, 0);
-	var e int = int(e1);
-	var fd int = int(r0);
-	if e == 0 {
-        scanner_test(fd);		
-        syscall.Syscall(syscall.SYS_CLOSE, uintptr(fd), 0, 0);
-	} else {
-		fmt.Printf("Error opening file %s.\n", os.Args[1]);
-	}
+//	var r0 uintptr;
+//	var e1 uintptr;
+//	r0, _, e1 = syscall.Syscall(syscall.SYS_OPEN, uintptr(unsafe.Pointer(syscall.StringBytePtr(os.Args[1]))), 0, 0);
+//	var e int = int(e1);
+//	var fd int = int(r0);
+    var fd uint64;
+    var test [255]byte;
+    libgogo.StringToByteBuf(os.Args[1],test);
+    fmt.Printf("%s\n",test);
+    fd = libgogo.FileOpen("gogo.go",0);
+    fmt.Printf("%d\n",fd);    
+	//if e == 0 {
+    //scanner_test(fd);		
+    libgogo.FileClose(fd);
+      //  syscall.Syscall(syscall.SYS_CLOSE, uintptr(fd), 0, 0);
+	//} else {
+	//	fmt.Printf("Error opening file %s.\n", os.Args[1]);
+	//}
 }
