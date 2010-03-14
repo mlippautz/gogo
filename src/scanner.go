067ca33a215720b9be126c175c07839c41713121
Michael Lippautz <michael.lippautz@gmail.com>
2010-03-15 00:05:48 +0100

    scanner: Add more commentary. Add multi-line comments.
    * Scanner now recognizes multi-line comments that use C++ syntax (/* ...
    */)

--- src/scanner.go
+++ src/scanner.go
@@ -4,53 +4,71 @@
 
 package main
 
+//
+// Imports
+// TODO: Get rid of all non libgogo ones.
+//
 import "fmt"
 import "./libgogo/_obj/libgogo"
 
-const TOKEN_IDENTIFIER uint64 = 1;
-const TOKEN_STRING = 2;
-const TOKEN_EOS = 3; // end of scan
-const TOKEN_LBRAC = 4;
-const TOKEN_RBRAC = 5;
-const TOKEN_LSBRAC = 6;
-const TOKEN_RSBRAC = 7;
-const TOKEN_INTEGER = 8;
-const TOKEN_LCBRAC = 9;
-const TOKEN_RCBRAC = 10;
-const TOKEN_PT = 11;
-const TOKEN_NOT = 12;
-const TOKEN_NOTEQUAL = 13;
-const TOKEN_SEMICOLON = 14;
-const TOKEN_COLON = 15;
-const TOKEN_ASSIGN = 16;
-const TOKEN_EQUALS = 17;
+//
+// Set of recognized tokens 
+//
+const TOKEN_IDENTIFIER uint64 = 1;  // Identifier
+const TOKEN_STRING = 2;             // String using "..."
+const TOKEN_EOS = 3;                // End of Scan
+const TOKEN_LBRAC = 4;              // Left bracket '('
+const TOKEN_RBRAC = 5;              // Right bracket ')'
+const TOKEN_LSBRAC = 6;             // Left square bracket '['
+const TOKEN_RSBRAC = 7;             // Right square bracket ']'
+const TOKEN_INTEGER = 8;            // Integer number
+const TOKEN_LCBRAC = 9;             // Left curly bracket '{'
+const TOKEN_RCBRAC = 10;            // Right curly bracket '}'
+const TOKEN_PT = 11;                // Point '.'
+const TOKEN_NOT = 12;               // Single not '!'
+const TOKEN_NOTEQUAL = 13;          // Comparison, not equal '!='
+const TOKEN_SEMICOLON = 14;         // Semi-colon ';'
+const TOKEN_COLON = 15;             // Colon ','
+const TOKEN_ASSIGN = 16;            // Assignment '='
+const TOKEN_EQUALS = 17;            // Equal comparison '=='
 
+//
+// Token struct holding the relevant data of a parsed token.
+//
 type Token struct {
-    id uint64;
-    value [255]byte;
-    value_len uint64;
+    id uint64; // The id. Is one of TOKEN_*
+    value [255]byte; // If the id requires a value to be stored, it is found here 
+    value_len uint64; // Length of the value stored in `value`
 
-    // also need a second token, because we may scan it already when terminating the first one
-    nextChar byte;    
+    nextChar byte; // Sometime the next char is already read. It is stored here to be re-assigned in the next GetNextToken() round
 };
 
-//
-// Function getting the next token.
-//
+/*
+ * Function getting the next token.
+ */
 func GetNextToken(fd uint64, oldToken Token) Token {
-    var singleChar byte;
+    var singleChar byte; // Byte holding the last read value
+    /* 
+     * Flag indicating whether we are in a comment.
+     * 0 for no comment
+     * 1 for a single line comment 
+     * 2 for a multi line comment
+     */
     var inComment uint64;
-    var done uint64;
-    var space_done uint64;
-    var newToken Token;
+    var done uint64; // Flag indicating whether a cycle (Token) is finsihed 
+    var spaceDone uint64; // Flag indicating whether an abolishment cycle is finished 
+    var newToken Token; // The new token that is returned
 
+    // Initialize variables
     done = 0;
-    space_done =0; 
+    spaceDone = 0;
     newToken.id = 0;
     newToken.value_len = 0;
     newToken.nextChar = 0; 
     inComment = 0;   
 
+    // If the old Token had to read the next char (and stored it), we can now
+    // get it back
     if oldToken.nextChar == 0 {       
         singleChar=libgogo.GetChar(fd)
     } else {
@@ -61,51 +79,83 @@ func GetNextToken(fd uint64, oldToken Token) Token {
     if singleChar == 0 {
         newToken.id = TOKEN_EOS;
         done = 1;
-        space_done = 1;
+        spaceDone = 1;
     }
 
-    // skip blank, newlines and comments
-    // after function singleChar contains the latest valid char
-    for ;space_done != 1; {
+    //
+    // Cleaning Tasks
+    // The next part strips out spaces, newlines, tabs, and comments
+    // Comments can either be single line with double slashes (//) or multiline
+    // using C++ syntax /* */ 
+    //
+    for ; spaceDone != 1; {
+
+        // check whether a comment is starting
         if singleChar == '/' {
+            // if we are in a comment skip the rest, get the next char otherwise
             if inComment == 0 {
                 singleChar = libgogo.GetChar(fd); 
                 if singleChar == '/' {
+                    // we are in a single line comment (until newline is found)
                     inComment = 1;
                 } else {
-                    tmp_error(">> Scanner: Unkown character combination for comments. Exiting.");
+                    if singleChar == '*' {
+                        // we are in a multiline comment (until ending is found)
+                        inComment = 2;
+                    } else {
+                        tmp_error(">> Scanner: Unkown character combination for comments. Exiting.");
+                    }
                 }
             }
-        } else {
-            // if character is a newline:
-            // 1) if in a comment, exit the comment
-            // 2) skip otherwise
-            if singleChar == 10 {
-                if inComment == 1 {
+        } 
+
+        // check whether a multi-line comment is ending
+        if singleChar == '*' {
+            singleChar = libgogo.GetChar(fd);
+            if singleChar == '/' {
+                if inComment == 2 {
                     inComment = 0;
-                } 
-            } else {
-                // skip spaces and tabs and handle everything else
-                if singleChar != ' ' && singleChar != 9 {
-                    // if not in a comment we have our current valid char
-                    if inComment == 0 {
-                        space_done = 1;
-                    } 
-                    // check if GetChar() returned EOF while skipping
-                    if singleChar == 0 {
-                        space_done = 1;
-                        newToken.id = TOKEN_EOS;
-                        done = 1;
-                    }   
+                    singleChar = libgogo.GetChar(fd);
                 }
             }
         }
-        if space_done == 0 {        
+
+        // if character is a newline:
+        //  *) if in a singleline comment, exit the comment
+        //  *) skip otherwise
+        if singleChar == 10 {
+            if inComment == 1 {
+                inComment = 0;
+            } 
+        } 
+
+        // handle everything that is not a space,tab,newline
+        if singleChar != ' ' && singleChar != 9 && singleChar != 10 {
+            // if not in a comment we have our current valid char
+            if inComment == 0 {
+                spaceDone = 1;
+            } 
+
+            // check if GetChar() returned EOF while skipping
+            if singleChar == 0 {
+                newToken.id = TOKEN_EOS;
+                spaceDone = 1;
+                done = 1;
+            }   
+        }
+    
+        
+        // if we are not done until now, get a new character and start another abolishing cycle        
+        if spaceDone == 0 {        
             singleChar=libgogo.GetChar(fd);
         }
     }
 
-    // catch identifiers.
+    //
+    // Actual scanning part starts here
+    //
+
+    // Catch identifiers
     // identifier = letter { letter | digit }.
     if (done != 1) && (singleChar >= 'A' && singleChar <= 'Z') || (singleChar >= 'a' && singleChar <= 'z') || singleChar == '_' { // check for letter or _
         newToken.id = TOKEN_IDENTIFIER;
