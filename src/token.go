32dd3419977da09316ad5ce8016634062de4f3a4
Michael Lippautz <michael.lippautz@gmail.com>
2010-03-21 19:30:17 +0100

    token.go: Comment and clean source file

--- src/token.go
+++ src/token.go
@@ -18,26 +18,29 @@ const TOKEN_INTEGER uint64 = 8;     // Integer number
 const TOKEN_LCBRAC uint64 = 9;      // Left curly bracket '{'
 const TOKEN_RCBRAC uint64 = 10;     // Right curly bracket '}'
 const TOKEN_PT uint64 = 11;         // Point '.'
-const TOKEN_NOT uint64 = 12;        // Single not '!'
+const TOKEN_NOT uint64 = 12;        // Boolean negation '!'
 const TOKEN_NOTEQUAL uint64 = 13;   // Comparison, not equal '!='
 const TOKEN_SEMICOLON uint64 = 14;  // Semi-colon ';'
 const TOKEN_COLON uint64 = 15;      // Colon ','
 const TOKEN_ASSIGN uint64 = 16;     // Assignment '='
 const TOKEN_EQUALS uint64 = 17;     // Equal comparison '=='
-const TOKEN_CHAR uint64 = 18;       // Single Quoted Character
-const TOKEN_REL_AND uint64 = 19;    // AND Relation
-const TOKEN_REL_OR uint64 = 20;     // OR Relation
-const TOKEN_REL_GTOE uint64 = 21;   // Greather-Than or Equal
-const TOKEN_REL_GT uint64 = 22;     // Greather-Than
-const TOKEN_REL_LTOE uint64 = 23;   // Less-Than or Equal
-const TOKEN_REL_LT uint64 = 24;     // Less-Than
-const TOKEN_ARITH_PLUS uint64 = 25; // Arith. Plus
-const TOKEN_ARITH_MINUS uint64 = 26;// Arith. Minus
-const TOKEN_ARITH_MUL uint64 = 27;  // Arith. Multiplication
-const TOKEN_ARITH_DIV uint64 = 28;  // Arith. Division
-const TOKEN_OP_ADR uint64 = 29;
+const TOKEN_CHAR uint64 = 18;       // Single Quoted Character 'x'
+const TOKEN_REL_AND uint64 = 19;    // AND Relation '&&'
+const TOKEN_REL_OR uint64 = 20;     // OR Relation '||'
+const TOKEN_REL_GTOE uint64 = 21;   // Greather-Than or Equal '>='
+const TOKEN_REL_GT uint64 = 22;     // Greather-Than '>'
+const TOKEN_REL_LTOE uint64 = 23;   // Less-Than or Equal '<='
+const TOKEN_REL_LT uint64 = 24;     // Less-Than '<'
+const TOKEN_ARITH_PLUS uint64 = 25; // Arith. Plus '+'
+const TOKEN_ARITH_MINUS uint64 = 26;// Arith. Minus '-'
+const TOKEN_ARITH_MUL uint64 = 27;  // Arith. Multiplication '*'
+const TOKEN_ARITH_DIV uint64 = 28;  // Arith. Division '/'
+const TOKEN_OP_ADR uint64 = 29;     // Address operator '&'
 
+//
 // Advanced tokens, that are generated in the 2nd step from identifiers
+// The tokens represent the corresponding language keywords.
+//
 const TOKEN_FOR uint64 = 101;
 const TOKEN_IF uint64 = 102;
 const TOKEN_TYPE uint64 = 103;
@@ -48,4 +51,3 @@ const TOKEN_RETURN uint64 = 107;
 const TOKEN_FUNC uint64 = 108;
 const TOKEN_PACKAGE uint64 = 109;
 const TOKEN_IMPORT uint64 = 110;
-
