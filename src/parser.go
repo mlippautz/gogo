36181ca2ad623acf54e2c7c17b607a4f804725c0
Michael Lippautz <michael.lippautz@gmail.com>
2010-04-11 12:46:20 +0200

    parser.go: Initial version of parser

--- src/parser.go
+++ src/parser.go
@@ -12,22 +12,40 @@ import "./libgogo/_obj/libgogo"
 // e .... array of expected tokens
 // eLen . actual length (items) of array
 //
-func parseError ( ue uint64, e [255]uint64, eLen uint64) {
+func parseError(ue uint64, e [255]uint64, eLen uint64) {
     var i uint64;
+    var str string;
 
     libgogo.PrintString(">> Parser: Unexpected token '");
-    libgogo.PrintString( TokenToString(ue) );   // maybe call in call will be supported
-    libgogo.PrintString("'.\n");
+    str = TokenToString(ue);
+    libgogo.PrintString(str);
+    libgogo.PrintString("' in line ");
+    libgogo.PrintNumber(lineCounter);
+    libgogo.PrintString(".\n");
     if eLen > 0 {
         libgogo.PrintString("           Expected one of: ");
-        libgogo.PrintString( TokenToString(e[i]) );
+        str = TokenToString(e[i]);
+        libgogo.PrintString(str);
         for i = 1; i < eLen; i = i+1 {
-            libgogo.PrintString( TokenToString(e[i]) );
+            str = TokenToString(e[i]);
+            libgogo.PrintString(str);
             libgogo.PrintString(", ");        
         }
         libgogo.PrintString("\n");
     }
     libgogo.Exit(2); 
+}
+
+//
+//
+//
+func GetNextTokenSafe(fd uint64, tok *Token) {
+    if tok.nextToken != 0 {
+        tok.id = tok.nextToken;      
+        tok.nextToken = 0;  
+    } else {
+        GetNextToken(fd, tok);
+    }    
 }
 
 
@@ -36,26 +54,195 @@ func parseError ( ue uint64, e [255]uint64, eLen uint64) {
 //
 func Parse( fd uint64 ) {
     var tok Token;
-    var es [255]uint64;
 
     tok.id = 0;
     tok.nextChar = 0;
-    
+    tok.nextToken = 0;    
 
-    //
-    // Check for package "identifier" as the golang forces this.
-    //
-    GetNextToken(fd,&tok);
-    if tok.id != TOKEN_PACKAGE {
-        es[0] = TOKEN_PACKAGE;
-        parseError(tok.id,es,1);
-    } 
-
-    // Parse the rest
-    // TBD
+    ParsePackageStatement(fd, &tok);    
+    ParseImportStatementList(fd, &tok);
+    ParseStructDeclList(fd, &tok);
 
     // Scan the rest for debugging purposes
     for GetNextToken(fd,&tok); tok.id != TOKEN_EOS; GetNextToken(fd,&tok) {
         debugToken(&tok);
     }
+}
+
+//
+// Check for package "identifier" as the golang forces this.
+//
+func ParsePackageStatement(fd uint64, tok *Token) {    
+    var es [255]uint64;
+
+    GetNextTokenSafe(fd,tok);
+    if tok.id != TOKEN_PACKAGE {
+        es[0] = TOKEN_PACKAGE;
+        parseError(tok.id,es,1);
+    } else {
+        GetNextTokenSafe(fd,tok);
+        if tok.id != TOKEN_IDENTIFIER {
+            es[0] = TOKEN_IDENTIFIER;
+            parseError(tok.id,es,1);
+        }
+    }
+    // package ok
+}
+
+//
+// Parses: { import_stmt }
+//
+func ParseImportStatementList(fd uint64, tok *Token) {
+    var boolFlag uint64;
+    for boolFlag = ParseImportStatement(fd, tok);boolFlag == 0;boolFlag = ParseImportStatement(fd, tok) {
+    }
+}
+
+
+//
+// Parses: "import" string
+//
+func ParseImportStatement(fd uint64, tok *Token) uint64 {
+    var es [255]uint64;
+    var boolFlag uint64;
+
+    GetNextTokenSafe(fd,tok);
+    if tok.id == TOKEN_IMPORT {
+        GetNextTokenSafe(fd, tok);
+        if tok.id != TOKEN_STRING  {
+            es[0] = TOKEN_STRING;
+            parseError(tok.id,es,1);
+        } else {
+            // import ok
+        }   
+        boolFlag = 0;
+    } else {
+        boolFlag = 1;
+        tok.nextToken = tok.id;
+    }    
+    return boolFlag;
+}
+
+//
+// Parses: { struct_decl }
+//
+func ParseStructDeclList(fd uint64, tok *Token) {
+    var boolFlag uint64;
+    for boolFlag = ParseStructDecl(fd, tok);boolFlag == 0;boolFlag = ParseStructDecl(fd, tok) {
+    }
+}
+
+//
+// Parses: "type" identifier "struct" "{" struct_var_decl_list "}" ";"
+//
+func ParseStructDecl(fd uint64, tok *Token) uint64 {
+    var es [255]uint64;
+    var boolFlag uint64;
+
+    GetNextTokenSafe(fd,tok);
+    if tok.id == TOKEN_TYPE {
+
+        GetNextTokenSafe(fd, tok);
+        if tok.id != TOKEN_IDENTIFIER  {
+            es[0] = TOKEN_IDENTIFIER;
+            parseError(tok.id,es,1);
+        }
+
+        GetNextTokenSafe(fd, tok);
+        if tok.id != TOKEN_STRUCT  {
+            es[0] = TOKEN_STRUCT;
+            parseError(tok.id,es,1);
+        }    
+
+        GetNextTokenSafe(fd, tok);
+        if tok.id != TOKEN_LCBRAC  {
+            es[0] = TOKEN_LCBRAC;
+            parseError(tok.id,es,1);
+        }
+
+        ParseStructVarDeclList(fd, tok);
+
+        GetNextTokenSafe(fd, tok);
+        if tok.id != TOKEN_RCBRAC  {
+            es[0] = TOKEN_RCBRAC;
+            parseError(tok.id,es,1);
+        }
+
+        GetNextTokenSafe(fd, tok);
+        if tok.id != TOKEN_SEMICOLON  {
+            es[0] = TOKEN_SEMICOLON;
+            parseError(tok.id,es,1);
+        }
+         
+        boolFlag = 0;
+    } else {
+        boolFlag = 1;
+        tok.nextToken = tok.id;
+    }    
+    return boolFlag;
+}
+
+//
+// Parses: { struct_var_decl }
+//
+func ParseStructVarDeclList(fd uint64, tok *Token) {
+    var boolFlag uint64;
+    for boolFlag = ParseStructVarDecl(fd, tok);boolFlag == 0;boolFlag = ParseStructVarDecl(fd, tok) {
+    }
+}
+
+//
+// Parses: identifier type ";"
+//
+func ParseStructVarDecl(fd uint64, tok *Token) uint64 {
+    var es [255]uint64;
+    var boolFlag uint64;
+
+    GetNextTokenSafe(fd,tok);
+    if tok.id == TOKEN_IDENTIFIER {
+
+        ParseType(fd, tok);
+
+        GetNextTokenSafe(fd, tok);
+        if tok.id != TOKEN_SEMICOLON  {
+            es[0] = TOKEN_SEMICOLON;
+            parseError(tok.id,es,1);
+        }
+        
+        boolFlag = 0;
+    } else {
+        boolFlag = 1;
+        tok.nextToken = tok.id;
+    }    
+    return boolFlag;    
+}
+
+//
+// Parses: [ "[" integer "]" ] identifier 
+//
+func ParseType(fd uint64, tok *Token) {
+    var es [255]uint64;
+
+    GetNextTokenSafe(fd, tok);
+    if tok.id == TOKEN_LSBRAC {        
+        GetNextTokenSafe(fd, tok);
+        if tok.id != TOKEN_INTEGER  {
+            es[0] = TOKEN_INTEGER;
+            parseError(tok.id,es,1);
+        }       
+
+        GetNextTokenSafe(fd, tok);
+        if tok.id != TOKEN_RSBRAC  {
+            es[0] = TOKEN_RSBRAC;
+            parseError(tok.id,es,1);
+        }
+    } else {
+        tok.nextToken = tok.id;
+    }
+
+    GetNextTokenSafe(fd, tok);
+    if tok.id != TOKEN_IDENTIFIER  {
+        es[0] = TOKEN_IDENTIFIER;
+        parseError(tok.id,es,0);
+    } 
 }
