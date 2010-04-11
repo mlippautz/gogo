10766f028fa7fac51ebcff1eb23073bb925f9368
Michael Lippautz <michael.lippautz@gmail.com>
2010-04-11 12:45:38 +0200

    token.go: Get rid of 'const' keywords as they are not used anymore

--- src/token.go
+++ src/token.go
@@ -7,50 +7,50 @@ package main
 //
 // Set of recognized tokens 
 //
-const TOKEN_IDENTIFIER uint64 = 1;  // Identifier
-const TOKEN_STRING uint64 = 2;      // String using "..."
-const TOKEN_EOS uint64 = 3;         // End of Scan
-const TOKEN_LBRAC uint64 = 4;       // Left bracket '('
-const TOKEN_RBRAC uint64 = 5;       // Right bracket ')'
-const TOKEN_LSBRAC uint64 = 6;      // Left square bracket '['
-const TOKEN_RSBRAC uint64 = 7;      // Right square bracket ']'
-const TOKEN_INTEGER uint64 = 8;     // Integer number
-const TOKEN_LCBRAC uint64 = 9;      // Left curly bracket '{'
-const TOKEN_RCBRAC uint64 = 10;     // Right curly bracket '}'
-const TOKEN_PT uint64 = 11;         // Point '.'
-const TOKEN_NOT uint64 = 12;        // Boolean negation '!'
-const TOKEN_NOTEQUAL uint64 = 13;   // Comparison, not equal '!='
-const TOKEN_SEMICOLON uint64 = 14;  // Semi-colon ';'
-const TOKEN_COLON uint64 = 15;      // Colon ','
-const TOKEN_ASSIGN uint64 = 16;     // Assignment '='
-const TOKEN_EQUALS uint64 = 17;     // Equal comparison '=='
-const TOKEN_CHAR uint64 = 18;       // Single Quoted Character 'x'
-const TOKEN_REL_AND uint64 = 19;    // AND Relation '&&'
-const TOKEN_REL_OR uint64 = 20;     // OR Relation '||'
-const TOKEN_REL_GTOE uint64 = 21;   // Greather-Than or Equal '>='
-const TOKEN_REL_GT uint64 = 22;     // Greather-Than '>'
-const TOKEN_REL_LTOE uint64 = 23;   // Less-Than or Equal '<='
-const TOKEN_REL_LT uint64 = 24;     // Less-Than '<'
-const TOKEN_ARITH_PLUS uint64 = 25; // Arith. Plus '+'
-const TOKEN_ARITH_MINUS uint64 = 26;// Arith. Minus '-'
-const TOKEN_ARITH_MUL uint64 = 27;  // Arith. Multiplication '*'
-const TOKEN_ARITH_DIV uint64 = 28;  // Arith. Division '/'
-const TOKEN_OP_ADR uint64 = 29;     // Address operator '&'
+var TOKEN_IDENTIFIER uint64 = 1;  // Identifier
+var TOKEN_STRING uint64 = 2;      // String using "..."
+var TOKEN_EOS uint64 = 3;         // End of Scan
+var TOKEN_LBRAC uint64 = 4;       // Left bracket '('
+var TOKEN_RBRAC uint64 = 5;       // Right bracket ')'
+var TOKEN_LSBRAC uint64 = 6;      // Left square bracket '['
+var TOKEN_RSBRAC uint64 = 7;      // Right square bracket ']'
+var TOKEN_INTEGER uint64 = 8;     // Integer number
+var TOKEN_LCBRAC uint64 = 9;      // Left curly bracket '{'
+var TOKEN_RCBRAC uint64 = 10;     // Right curly bracket '}'
+var TOKEN_PT uint64 = 11;         // Point '.'
+var TOKEN_NOT uint64 = 12;        // Boolean negation '!'
+var TOKEN_NOTEQUAL uint64 = 13;   // Comparison, not equal '!='
+var TOKEN_SEMICOLON uint64 = 14;  // Semi-colon ';'
+var TOKEN_COLON uint64 = 15;      // Colon ','
+var TOKEN_ASSIGN uint64 = 16;     // Assignment '='
+var TOKEN_EQUALS uint64 = 17;     // Equal comparison '=='
+var TOKEN_CHAR uint64 = 18;       // Single Quoted Character 'x'
+var TOKEN_REL_AND uint64 = 19;    // AND Relation '&&'
+var TOKEN_REL_OR uint64 = 20;     // OR Relation '||'
+var TOKEN_REL_GTOE uint64 = 21;   // Greather-Than or Equal '>='
+var TOKEN_REL_GT uint64 = 22;     // Greather-Than '>'
+var TOKEN_REL_LTOE uint64 = 23;   // Less-Than or Equal '<='
+var TOKEN_REL_LT uint64 = 24;     // Less-Than '<'
+var TOKEN_ARITH_PLUS uint64 = 25; // Arith. Plus '+'
+var TOKEN_ARITH_MINUS uint64 = 26;// Arith. Minus '-'
+var TOKEN_ARITH_MUL uint64 = 27;  // Arith. Multiplication '*'
+var TOKEN_ARITH_DIV uint64 = 28;  // Arith. Division '/'
+var TOKEN_OP_ADR uint64 = 29;     // Address operator '&'
 
 //
 // Advanced tokens, that are generated in the 2nd step from identifiers
 // The tokens represent the corresponding language keywords.
 //
-const TOKEN_FOR uint64 = 101;
-const TOKEN_IF uint64 = 102;
-const TOKEN_TYPE uint64 = 103;
-const TOKEN_CONST uint64 = 104;
-const TOKEN_VAR uint64 = 105;
-const TOKEN_STRUCT uint64 = 106;
-const TOKEN_RETURN uint64 = 107;
-const TOKEN_FUNC uint64 = 108;
-const TOKEN_PACKAGE uint64 = 109;
-const TOKEN_IMPORT uint64 = 110;
+var TOKEN_FOR uint64 = 101;
+var TOKEN_IF uint64 = 102;
+var TOKEN_TYPE uint64 = 103;
+var TOKEN_CONST uint64 = 104;
+var TOKEN_VAR uint64 = 105;
+var TOKEN_STRUCT uint64 = 106;
+var TOKEN_RETURN uint64 = 107;
+var TOKEN_FUNC uint64 = 108;
+var TOKEN_PACKAGE uint64 = 109;
+var TOKEN_IMPORT uint64 = 110;
 
 //
 // Helper functions
