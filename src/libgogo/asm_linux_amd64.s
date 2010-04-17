f4611c2c9af0f2628c664cc77398a90e70776bd4
Andreas Unterweger <andi_uw@gmx.at>
2010-04-17 23:33:48 +0200

    libgogo.go: Switch to use of original Plan9 assembler syntax in order to avoid various stack corruption errors related to the use of custom assembler functions (also tried with a corrupted older revision which failed on parsing scanner.go)

--- src/libgogo/asm_linux_amd64.s
+++ src/libgogo/asm_linux_amd64.s
@@ -2,31 +2,31 @@
 // GoGo Library functions (ASM)
 //
 
-TEXT ·StringLength(SB),3,$0 //StringLength: 1 parameter, 1 return value
+TEXT ·StringLength(SB),$0-24 //StringLength: 1 parameter (2), 1 return value
   MOVQ $0, 24(SP) //Set return value to 0
   MOVW 16(SP), AX //String length is stored together with the string (first parameter = SP+64bit -> +64bit = SP+2*64bit)
   MOVW AX, 24(SP) //Move length to result with only 32 bits (return value after one parameter => SP+3*64bit)
   RET
 
-TEXT ·ToIntFromByte(SB),2,$0 //ToIntFromByte: 1 parameter, 1 return value
+TEXT ·ToIntFromByte(SB),$0-16 //ToIntFromByte: 1 parameter, 1 return value
   MOVQ $0, AX //Set AX to 0
   MOVB 8(SP), AL //Move byte parameter to AL (first parameter => SP+64bit)
   MOVQ AX, 16(SP) //Move whole AX register with byte parameter to result (return value after one parameter => SP+2*64bit)
   RET
 
-TEXT ·ToByteFromInt(SB),2,$0 //ToByteFromInt: 1 parameter, 1 return value
+TEXT ·ToByteFromInt(SB),$0-16 //ToByteFromInt: 1 parameter, 1 return value
   MOVQ $0, 16(SP) //Clear whole return value (return value after one parameter => SP+2*64bit)
   MOVQ 8(SP), AX //Move whole parameter to AX (first parameter => SP+64bit)
   MOVB AL, 16(SP) //Move AL (last byte of parameter) to result (return value after one parameter => SP+2*64bit)
   RET
 
-TEXT ·Exit(SB),1,$0 //Exit: 1 parameter, no return value
+TEXT ·Exit(SB),$0-8 //Exit: 1 parameter, no return value
   MOVQ $60, AX //sys_exit (1 parameter)
   MOVQ 8(SP), DI //return code (first parameter => SP+1*64bit)
   SYSCALL //Linux syscall
   RET //Just to be sure (should never be reached)
 
-TEXT ·Write(SB),5,$0 //Write: 3 parameters, 1 return value
+TEXT ·Write(SB),$0-40 //Write: 3 parameters (4), 1 return value
   MOVQ $1, AX //sys_write (3 parameters)
   MOVQ 8(SP), DI //fd (first parameter => SP+64bit)
   MOVQ 16(SP), SI //text (second parameter => SP+2*64bit)
@@ -41,7 +41,7 @@ WRITE_SUCCESS:
   MOVQ AX, 40(SP) //First return value of syscall is in AX (return value after three parameters => SP+5*64bit)
   RET
 
-TEXT ·PrintChar(SB),1,$0 //PrintChar: 1 parameter, no return value
+TEXT ·PrintChar(SB),$0-8 //PrintChar: 1 parameter, no return value
   MOVQ $1, AX //sys_write (3 parameters)
   MOVQ $1, DI //fd (1 = stdout)
   LEAQ 8(SP), SI //text (address of second parameter => SP+64bit)
@@ -49,7 +49,7 @@ TEXT ·PrintChar(SB),1,$0 //PrintChar: 1 parameter, no return value
   SYSCALL //Linux syscall
   RET
 
-TEXT ·Read(SB),5,$0 //Read: 3 parameters, 1 return value
+TEXT ·Read(SB),$0-40 //Read: 3 parameters (4), 1 return value
   MOVQ $0, AX //sys_read (3 parameters)
   MOVQ 8(SP), DI //fd (first parameter => SP+64bit)
   MOVQ 16(SP), SI //buffer (second parameter => SP+2*64bit)
@@ -64,7 +64,7 @@ READ_SUCCESS:
   MOVQ AX, 40(SP) //First return value of syscall is in AX (return value after three parameters => SP+5*64bit)
   RET
 
-TEXT ·GetChar(SB),2,$0 //Read: 1 parameter, 1 return value
+TEXT ·GetChar(SB),$0-16 //Read: 1 parameter (2), 1 return value
   MOVQ $0, AX //sys_read (3 parameters)
   MOVQ 8(SP), DI //fd (first parameter => SP+64bit)
   MOVQ $0, 16(SP) //Initialize result with 0
@@ -78,7 +78,7 @@ GETCHAR_ERROR:
 GETCHAR_SUCCESS:
   RET
 
-TEXT ·FileOpen(SB),4,$0 //FileOpen: 2 parameters, 1 return value
+TEXT ·FileOpen(SB),$0-32 //FileOpen: 2 parameters (3), 1 return value
   MOVQ $2, AX //sys_open (2 parameters)
   MOVQ 8(SP), DI //filename (first parameter => SP+64bit)
   MOVQ 24(SP), SI //flags (second parameter => SP+3*64bit)
@@ -93,7 +93,7 @@ FILEOPEN_SUCCESS:
   MOVQ AX, 32(SP) //First return value of syscall is in AX (return value after three parameters => SP+4*64bit)
   RET
 
-TEXT ·FileClose(SB),2,$0 //FileClose: 1 parameter, 1 return value
+TEXT ·FileClose(SB),$0-16 //FileClose: 1 parameter, 1 return value
   MOVQ $3, AX //sys_close (3 parameters)
   MOVQ 8(SP), DI //filename (first parameter => SP+64bit)
   MOVQ $0, SI //not used
