f4fb74f0d0e541f59ea404143ced9acbbd9f21df
Michael Lippautz <michael.lippautz@gmail.com>
2010-04-18 14:47:29 +0200

    libgogo_test: Fix StringLength checking and add StringCompare checking

--- src/libgogo/libgogo_test.go
+++ src/libgogo/libgogo_test.go
@@ -1,9 +1,16 @@
+// Copyright 2010 The GoGo Authors. All rights reserved.
+// Use of this source code is governed by the MIT
+// license that can be found in the LICENSE file.
 
 package libgogo
 
 import "testing"
 import "./_obj/libgogo"
 
+//
+// Perform some basic checks on the libgogo.StringLength function
+// This function is not capable of handling utf strings!
+//
 func TestStringLength(t *testing.T) { 
     var len uint64;
 
@@ -22,9 +29,26 @@ func TestStringLength(t *testing.T) {
 		t.Fatalf("libgogo.StringLength() = %d, want 100", len)
     }
 
-    len = libgogo.StringLength("+-*/&|!{}[].,;=<>");
-    if len != 17 {
-		t.Fatalf("libgogo.StringLength() = %d, want 17", len)
+    len = libgogo.StringLength("+-*/><=!&.");
+    if len != 10 {
+		t.Fatalf("libgogo.StringLength() = %d, want 10", len)
     } 
+}
+
+//
+// Perform basic comparison checks on libgogo.StringCompare function
+//
+func TestStringCompare(t *testing.T)  {
+    var cmpFlag uint64;
+    
+    cmpFlag = libgogo.StringCompare("a","a");
+    if cmpFlag != 0 {
+        t.Fatalf("libgogo.StringCompare(\"a\") != 0, received %d", cmpFlag);
+    }
+
+    cmpFlag = libgogo.StringCompare("these strings","are not equal");
+    if cmpFlag == 0 {
+        t.Fatalf("libgogo.StringCompare(\"these strings\",\"are not equal\") == 0");
+    }
 }
 
